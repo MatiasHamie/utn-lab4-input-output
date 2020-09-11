d98001aef9338e0efdd76bd4b1a77404614b1ef2
MatiasHamie <mnhamie@gmail.com>
2020-09-10 21:23:14 -0300

    Segunda parte del ejercicio

--- src/app/componentes/listado-entidad/listado-entidad.component.ts
+++ src/app/componentes/listado-entidad/listado-entidad.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-listado-entidad',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listado-entidad.component.css']
 })
 export class ListadoEntidadComponent implements OnInit {
+
+  @Input() datosMockaroo: any;
 
   constructor() { }
 
